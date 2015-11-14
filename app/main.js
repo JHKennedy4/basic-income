751480e044501137f401b53731ba545752cdf9fa
John Kennedy <jkennedy@fitbit.com>
2015-11-14 15:13:49 -0800

    font and framing

--- app/main.js
+++ app/main.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
+import Tax from './tax-calculator'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
