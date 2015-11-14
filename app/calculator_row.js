85b9e8cb9277c4c6cd95bc288f323f396e2d232b
Tom Dooner <tom.dooner@brigade.com>
2015-11-14 12:17:53 -0800

    Add some .calculator-row styling

--- app/calculator_row.js
+++ app/calculator_row.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default class CalculatorRow extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-xs-2">{this.props.title}</div>
+      <div className="row calculator-row">
+        <div className="col-xs-2 calculator-row-title"><h3>{this.props.title}</h3></div>
         {this.props.children}
       </div>
     );
