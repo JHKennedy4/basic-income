2b99bb40c1bb46bdb1bdc14ee5c641c5e01c1805
Tom Dooner <tom.dooner@brigade.com>
2015-11-14 11:48:50 -0800

    Add a basic DOM bar graph to <Footer>

--- app/footer.js
+++ app/footer.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default class Footer extends React.Component {
   render () {
+    var barWidthPct = this.props.incomeAmount / this.props.barMaxAmount * 100;
+    var barStyle = {
+      backgroundColor: 'green',
+      width: barWidthPct.toString() + '%',
+      height: '20px'
+    }
+    var povertyLineStyle = {
+      marginLeft: (this.props.barPovertyLineAmount / this.props.barMaxAmount * 100).toString() + '%',
+      width: '100%'
+    }
+    var livingWageStyle = {
+      marginLeft: (this.props.barLivingWageAmount / this.props.barMaxAmount * 100).toString() + '%',
+      width: '100%'
+    }
+
     return (
       <footer key="footer" className='calculator-footer'>
         <div className="container">
@@ -10,7 +25,9 @@ export default class Footer extends React.Component {
               Your Basic Income:
             </div>
             <div className="col-xs-8">
-              [bar chart]
+              <div style={barStyle}></div>
+              <div style={povertyLineStyle}>↑ poverty line</div>
+              <div style={livingWageStyle}>↑ living wage</div>
             </div>
             <div className="col-xs-2">
               ${this.props.incomeAmount} / mo.
@@ -21,4 +38,9 @@ export default class Footer extends React.Component {
       )
   }
 }
-Footer.defaultProps = { incomeAmount: 10000 }
+Footer.defaultProps = {
+  barMaxAmount: 40000,
+  barPovertyLineAmount: 16000,
+  barLivingWageAmount: 25000,
+  incomeAmount: 10000,
+}
