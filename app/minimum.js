5f30af48ca53be599287b5fac6df13d69a5fcac8
rohit.kumar <rohit.kumar@workday.com>
2015-11-14 15:47:16 -0800

    add clean income for reals

--- app/minimum.js
+++ app/minimum.js
@@ -1,22 +1,35 @@
 import d3 from 'd3'
 import React from 'react'
-import raw_income_data from './income.js'
+import raw_income_data from './clean_income.js'
 
 export default class Min extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      income_data: JSON.parse(raw_income_data),
+      income_data: raw_income_data,
     }
     //this.loadDataFromServer = this.loadDataFromServer.bind(this);
+  }
+
+  calculate_cost(basic_income) {
+    var income_range_index = 0;
+    var sum_households_below = 0;
+    raw_income_data.forEach(function (element, index) {
+      sum_households_below = sum_households_below + 
+      if (parseInt(element["income_range"]) <= basic_income) {
+        income_range_index = index
+      }
+    });
   }
 
 
   render() {
     return (
-      <div className='someCss.test'>
+     /* <div className='someCss.test'>
       {JSON.stringify(this.state.income_data)}
       </div>
+      */
+     <input type="text" className="form-control" />
     );
   }
 
