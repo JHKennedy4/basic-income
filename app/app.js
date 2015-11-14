920aecf3f03a198a05e8a88225838dcfa2ec0516
Tom Dooner <tom.dooner@brigade.com>
2015-11-14 11:55:51 -0800

    Add some examples of the UI elements

--- app/app.js
+++ app/app.js
@@ -5,15 +5,57 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        <header className='calculator-header'>
-          <div className="container">
-            <div className="row">
-              <div className="col-xs-12">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <header className='calculator-header'>
                 <h1>Basic Income Calculator</h1>
-              </div>
+              </header>
             </div>
           </div>
-        </header>
+
+          <div className="row">
+            <div className="col-xs-2">Tax Brackets</div>
+            <div className="col-xs-2">
+              <ul>
+                <li>$10,000 - 10%</li>
+                <li>$20,000 - 14%</li>
+                <li>$50,000 - 20%</li>
+                <li>$100,000 - 30%</li>
+              </ul>
+            </div>
+            <div className="col-xs-2">
+              <ul>
+                <li>$10,000 - 14%</li>
+                <li>$20,000 - 18%</li>
+                <li>$50,000 - 24%</li>
+                <li>$100,000 - 35%</li>
+              </ul>
+            </div>
+            <div className="col-xs-2">
+              <ul>
+                <li>$10,000 - 0%</li>
+                <li>$20,000 - 7%</li>
+                <li>$50,000 - 14%</li>
+                <li>$100,000 - 33%</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-2">Minimum Income</div>
+            <div className="col-xs-2">$10,000</div>
+            <div className="col-xs-2">$15,000</div>
+            <div className="col-xs-2">$20,000</div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-2">Welfare Programs</div>
+            <div className="col-xs-2">End them all!</div>
+            <div className="col-xs-2">End some of them.</div>
+            <div className="col-xs-2">Keep them.</div>
+          </div>
+        </div>
 
         <Footer incomeAmount={15000} />
       </div>
