63091ed0ea3156a6cb63b22448692a1a8653dbb2
Tom Dooner <tom.dooner@brigade.com>
2015-11-14 11:25:00 -0800

    Add basic skeleton and bootstrap

--- app/app.js
+++ app/app.js
@@ -3,8 +3,32 @@ import React from 'react'
 export default class App extends React.Component {
   render () {
     return (
-      <div className='someCss.test'>
-        Hello, again my friend!
+      <div>
+        <header className='calculator-header'>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12">
+                <h1>Basic Income Calculator</h1>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <footer key="footer" className='calculator-footer'>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-2">
+                Your Basic Income:
+              </div>
+              <div className="col-xs-8">
+                [bar chart]
+              </div>
+              <div className="col-xs-2">
+                $10,000 / mo.
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     )
   }
