866bc559ea3ce7f3743262058dbf08c81c919533
Tom Dooner <tom.dooner@brigade.com>
2015-11-14 17:04:37 -0800

    Add revenues data

---
+++ app/revenues.js
@@ -0,0 +1,18 @@
+// for 2013
+// numbers from: http://www.gpo.gov/fdsys/pkg/BUDGET-2016-DB/xls/BUDGET-2016-DB-3.xls
+var revenues = {
+  // FOASI, Transfers from General Fund (FICA Taxes)
+  // FOASI, Transfers from General Fund (SECA Taxes)
+  social_security: 545045000 + 32598000,
+
+  // FHI Trust Fund, Transfers from General Fund (FICA Taxes)
+  // FHI Trust Fund, Transfers from General Fund (SECA Taxes)
+  // FDI, Transfers from General Fund (FICA Taxes)
+  // FDI, Transfers from General Fund (SECA Taxes)
+  medicare: 192707000 + 16016000 + 92540000 + 5534000,
+
+  // Unemployment Trust Fund, State Accounts, Deposits by States
+  // General Taxes, FUTA, Unemployment Trust Fund
+  // Taxes, Rail Industry Pension Fund
+  unemployment: 48952000 + 7748000 + 2822000
+}
