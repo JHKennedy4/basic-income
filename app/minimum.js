b28e9834ca8a84d44c80862d0076389f57489972
rohit.kumar <rohit.kumar@workday.com>
2015-11-14 16:22:00 -0800

    initial calc

--- app/minimum.js
+++ app/minimum.js
@@ -14,22 +14,32 @@ export default class Min extends React.Component {
   calculate_cost(basic_income) {
     var income_range_index = 0;
     var sum_households_below = 0;
-    raw_income_data.forEach(function (element, index) {
-      sum_households_below = sum_households_below + 
-      if (parseInt(element["income_range"]) <= basic_income) {
+    var sum_income_delta = 0;
+    for (var index = 0; index < raw_income_data.length; index++) {
+      var element = raw_income_data[index];
+      var households = parseInt(element["households_thousands"])
+      sum_households_below = sum_households_below + households
+      var income_range = parseInt(element["income_range"])
+      sum_income_delta = sum_income_delta + ((basic_income - income_range) * households * 1000)
+      if (income_range <= basic_income) {
         income_range_index = index
+        return sum_income_delta;
       }
-    });
+    }
   }
 
-
   render() {
+    var x = this.calculate_cost(30000);
+    console.log("val: " + x);
     return (
      /* <div className='someCss.test'>
       {JSON.stringify(this.state.income_data)}
       </div>
       */
-     <input type="text" className="form-control" />
+     <div>
+      <input type="text" className="form-control" />
+      <p>{this.calculate_cost(30000)}</p>
+     </div>
     );
   }
 
